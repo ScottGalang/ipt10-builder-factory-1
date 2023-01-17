c33fc3f7066235037b0379a861c7e8beb424e62a
ScottGalang <galang.scottrylee@auf.edu.ph>
2023-01-17 16:51:42 +0800

    first commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'c35db1f6246fa59c8357f9e68b42987911db4e4e',
+        'name' => 'ryzen/builderfactory1',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'ryzen/builderfactory1' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'c35db1f6246fa59c8357f9e68b42987911db4e4e',
+            'dev_requirement' => false,
+        ),
+    ),
+);
